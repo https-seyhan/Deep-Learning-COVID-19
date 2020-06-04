7e113baa3a4dc371d70011d6f22bad01597fb964
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2020-06-04 22:41:27 +1000

    Add files via upload

--- protein_prediction.py
+++ protein_prediction.py
@@ -1,5 +1,6 @@
 from Bio.PDB import *
 import os
+import collections
 os.chdir('/home/saul/protein/544698_993839_bundle_archive/structures_4_3_2020')
 import xpdb   # this is the module described below
 import pandas as pd
@@ -18,17 +19,30 @@ atomlist = []
 
 for atom in atoms:
     #print(type(atom))
-    lt = lt + 1
     atomlist.append(atom)
-
-#print(Counter(atomlist))
 
 atomdict = Counter(atomlist)
 
-print(atomdict)
+atoms = []
 for item, val in enumerate(atomdict):
-   print(" Item {} has the value of {}".format(item, val))
+   #print(" Item {} has the value of {}".format(item, val))
+   value = str(val).strip('<>Atom ')
+   atoms.append(value)
+   #print(value)
+   atoms.append(value)
 
-atomdist = [len(list(group)) for key, group in groupby(atomlist)]
-#print(atomdist)
-#print(atomlist)
+atomfreq=collections.Counter(atoms)
+#print(atomfreq)
+
+sortedatomfreq = {k: v for k, v in sorted(atomfreq.items(), key=lambda item: item[1], reverse=True)}
+
+#Print Atom frequency by descented order
+
+for item, val in enumerate(sortedatomfreq):
+    #print(" Item {} has the value of {}".format(item, val))
+    print(" Atom {} has the value of {}".format(val, atomfreq[val]))
+    #print(" Item {} has the value of {}".format(item, val))
+
+
+
+
