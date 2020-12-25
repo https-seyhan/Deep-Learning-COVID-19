2b0efe4736c8401b1aca43c68176732ceae10a59
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2020-12-25 11:08:29 +1100

    Update protein_prediction_atoms_Object_Oriented.py

--- protein_prediction_atoms_Object_Oriented.py
+++ protein_prediction_atoms_Object_Oriented.py
@@ -26,8 +26,6 @@ sloppyparser = PDBParser(PERMISSIVE=True,
 class Atoms:
 
     def __init__(self):
-
-        
         self.PL_PRO_C_terminal = pd.DataFrame(columns=['Atom', 'freq'])
         self.nsp2 = pd.DataFrame(columns=['Atom', 'freq'])
         self.nsp4= pd.DataFrame(columns=['Atom', 'freq'])
