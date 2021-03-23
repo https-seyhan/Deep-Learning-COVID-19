6a6d6b2dc03dffb0f2adc4d55de3b5f97f66d13b
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-23 21:05:06 +1100

    Update proteinHeatMap.py

--- proteinHeatMap.py
+++ proteinHeatMap.py
@@ -22,7 +22,7 @@ def plotHeadMap():
 	#print(heatdata.head())
 	print(type(heatdata['freq']))
 	heatdata['freq'] = pd.to_numeric(heatdata['freq'],  downcast='signed')
-	#f, ax = plt.subplots(figsize=(200, 200))
+	
 	sb.heatmap(pivotdata, annot=True, linewidths=.7, fmt='g', cmap='tab10', ax=ax)
 	plt.show()
 
