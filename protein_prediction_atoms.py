fa7f54d24c195219882c7ba885bae49a03c70126
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-25 19:56:29 +1100

    Update protein_prediction_atoms.py

--- protein_prediction_atoms.py
+++ protein_prediction_atoms.py
@@ -10,7 +10,6 @@ import glob
 location = '/home/saul/protein/544698_993839_bundle_archive/structures_4_3_2020'
 os.chdir(location)
 
-
 sloppyparser = PDBParser(PERMISSIVE=True,
                          structure_builder=xpdb.SloppyStructureBuilder())
 
