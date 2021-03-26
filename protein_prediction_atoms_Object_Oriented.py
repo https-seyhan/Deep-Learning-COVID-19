ff75840777da28c252e1aa210e34789a38b37203
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-27 10:14:04 +1100

    Update protein_prediction_atoms_Object_Oriented.py

--- protein_prediction_atoms_Object_Oriented.py
+++ protein_prediction_atoms_Object_Oriented.py
@@ -57,7 +57,7 @@ class Atoms:
             value = str(val).strip('<>Atom ')
             atoms.append(value)
             
-            #atoms.append(value)
+            
         atomfreq = collections.Counter(atoms) # count number of occurrences of atoms
         sortedatomfreq = {k: v for k, v in sorted(atomfreq.items(), key=lambda item: item[1], reverse=True)}
         #print(sortedatomfreq)
