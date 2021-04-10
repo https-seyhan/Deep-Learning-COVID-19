29ef90906217f4f4bbb9e9ed10734ad079cdaa4a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-10 11:39:21 +1000

    Update protein_prediction_atoms_Object_Oriented.py

--- protein_prediction_atoms_Object_Oriented.py
+++ protein_prediction_atoms_Object_Oriented.py
@@ -14,7 +14,6 @@ from Bio.PDB import *
 
 location = '/home/saul/protein/544698_993839_bundle_archive/structures_4_3_2020'
 os.chdir(location)
-
 # This code gets the frequency distribution of each atoms in all protein types and outputs them to CSV files.
 # read the structure
 sloppyparser = PDBParser(PERMISSIVE=True,
